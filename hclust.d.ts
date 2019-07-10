f50d8200f4cc9e04b23a61c840cd52243fa2df96
Michaël Zasso <targos@protonmail.com>
2019-07-10 09:23:36 +0200

    fix: correct TS definitions

--- hclust.d.ts
+++ hclust.d.ts
@@ -1,35 +1,35 @@
-declare module 'ml-hclust' {
-  export type AgglomerationMethod =
-    | 'single'
-    | 'complete'
-    | 'average'
-    | 'centroid'
-    | 'ward';
-  export interface AgnesOptions {
-    distanceFunction: (a: T, b: T) => number;
-    method: AgglomerationMethod;
-    isDistanceMatrix: boolean;
-  }
+export type AgglomerationMethod =
+  | 'single'
+  | 'complete'
+  | 'average'
+  | 'centroid'
+  | 'ward';
 
-  export interface DianaOptions<T> {
-    distanceFunction: (a: T, b: T) => number;
-  }
-
-  export interface Cluster {
-    children: Cluster[];
-    distance: number;
-    index: number[];
-    cut: (threshold: number) => Cluster[];
-    group: (minGroups: number) => Cluster;
-    traverse: (cb: (cluster: Cluster) => void) => void;
-  }
-
-  export function agnes<T = number[]>(
-    data: T[],
-    options?: AgnesOptions<T>
-  ): Cluster;
-  export function diana<T = number[]>(
-    data: T[],
-    options?: DianaOptions<T>
-  ): Cluster;
+export interface AgnesOptions<T> {
+  distanceFunction: (a: T, b: T) => number;
+  method: AgglomerationMethod;
+  isDistanceMatrix: boolean;
 }
+
+export interface DianaOptions<T> {
+  distanceFunction: (a: T, b: T) => number;
+}
+
+export interface Cluster {
+  children: Cluster[];
+  distance: number;
+  index: number[];
+  cut: (threshold: number) => Cluster[];
+  group: (minGroups: number) => Cluster;
+  traverse: (cb: (cluster: Cluster) => void) => void;
+}
+
+export function agnes<T = number[]>(
+  data: T[],
+  options?: AgnesOptions<T>
+): Cluster;
+
+export function diana<T = number[]>(
+  data: T[],
+  options?: DianaOptions<T>
+): Cluster;
